668ca626a886c0307ae8e7be95a126cd355c293b
spaghetti-lover <phungducanh2511@gmail.com>
2025-11-13 17:40:56 +0700

    feat: done handle request line

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net"
 
-	"github.com/spaghetti-lover/go-http/internal/utils"
+	"github.com/spaghetti-lover/go-http/internal/request"
 )
 
 func main() {
@@ -24,31 +24,39 @@ func main() {
 			continue
 		}
 
-		go handleConnection(conn)
-	}
-}
-
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-
-	log.Printf("New connection from %s", conn.RemoteAddr())
-
-	lineChannel, errorChannel := utils.GetLinesChannel(conn)
-
-	for {
-		select {
-		case line, ok := <-lineChannel:
-			if !ok {
-				log.Printf("Connection closed from %s", conn.RemoteAddr())
-				return
-			}
-			fmt.Printf("read: %s\n", line)
-
-		case err := <-errorChannel:
-			if err != nil {
-				log.Printf("Error reading from %s: %v", conn.RemoteAddr(), err)
-				return
-			}
+		r, err := request.FromReader(conn)
+		if err != nil {
+			log.Fatalf("Error reading from %s: %v", conn.RemoteAddr(), err)
 		}
+
+		fmt.Printf("Request line:\n")
+		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
 	}
 }
+
+// func handleConnection(conn net.Conn) {
+// 	defer conn.Close()
+
+// 	log.Printf("New connection from %s", conn.RemoteAddr())
+
+// 	lineChannel, errorChannel := utils.GetLinesChannel(conn)
+
+// 	for {
+// 		select {
+// 		case line, ok := <-lineChannel:
+// 			if !ok {
+// 				log.Printf("Connection closed from %s", conn.RemoteAddr())
+// 				return
+// 			}
+// 			fmt.Printf("read: %s\n", line)
+
+// 		case err := <-errorChannel:
+// 			if err != nil {
+// 				log.Printf("Error reading from %s: %v", conn.RemoteAddr(), err)
+// 				return
+// 			}
+// 		}
+// 	}
+// }
